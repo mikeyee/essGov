93a7d061cba8102334c499f673f8a1fb52f241f7
Mike Yee <yeekafai1@gmail.com>
2020-07-26 16:56:04 +0800

    use redirect to root instead of 2 render

--- index.js
+++ index.js
@@ -10,43 +10,55 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 
-var dataArray;
+const maxNo = 50;
+var dataArray = [];
+var answer;
 var searchTitle;
 var noResult;
+var keyword;
+var sortCriteria = 'subsidy';
+var totalSubsidy;
+var totalStaff;
 
 app.get('/', function(req, res){
-	dataArray = [];
-	fs.createReadStream('allData.csv')
-	.pipe(csv())
-	.on('data', (row) => {
-		dataArray.push(row);
-	})
-	.on('end', () => {
-		console.log('CSV file successfully processed');
-		res.render("list", {companyRecords: [], searchTitle:'', noResult:''});
-	});
-	
+	if (Array.isArray(dataArray) && dataArray.length) {
+		res.render('list', {companyRecords: answer, 
+						searchTitle: searchTitle,
+						noResult: noResult,
+						lastKeyword: keyword,
+						sortCriteria: sortCriteria,
+						totalSubsidy: totalSubsidy,
+						totalStaff: totalStaff
+					});
+	} else {
+		fs.createReadStream('allData.csv')
+		.pipe(csv())
+		.on('data', (row) => {
+			dataArray.push(row);
+		})
+		.on('end', () => {
+			console.log('CSV file successfully processed');
+			res.render("list", {companyRecords: [], searchTitle:'', noResult:'', lastKeyword:'', sortCriteria: "subsidy",
+				 totalSubsidy: 0, totalStaff: 0});
+		});
+	}
 });
 
 app.post('/', function(req, res){
-	var keyword = req.body.keyword;
-	var keywordArr = keyword.split(',');
-	var trimKeywordArr = keywordArr.map(s => s.trim());
-	var lang = req.body.submit;
-	var answer = [];
-	var datum;
+
+	answer = [];
+	sortCriteria = req.body.optradio;
+	keyword = req.body.keyword;
+	let keywordArr = keyword.split(',');
+	let trimKeywordArr = keywordArr.map(s => s.trim());
+	let lang = req.body.submit;
 
 	console.log(req.body);
 	console.log(dataArray.length);
 	
 	if (keyword !== '') {
-		for (datum of dataArray){
-			var companyName;
-			if (lang === 'en') {
-				companyName = datum.companyEn;
-			} else {
-				companyName = datum.companyCh;
-			}
+		for (let datum of dataArray){
+			let companyName = (lang === 'en') ? datum.companyEn : datum.companyCh;
 			for(let keyword of trimKeywordArr){
 				if (companyName.toLowerCase().indexOf(keyword.toLowerCase()) !== -1) {
 					answer.push(datum);
@@ -55,44 +67,42 @@ app.post('/', function(req, res){
 			}
 		}
 
+		answer = sortAnswer(answer, sortCriteria);
+
 		searchTitle = '關鍵詞： ' + keyword + ' ';
 		noResult = answer.length;
 
 	} else {
 
-		const maxNo = 50;
-
-		dataArray1 = JSON.parse(JSON.stringify(dataArray));
-		answer = dataArray1.sort(function (x, y) {
-		    return y.subsidy - x.subsidy;
-		});
+		answer = JSON.parse(JSON.stringify(dataArray));
+		answer = sortAnswer(answer, sortCriteria);
 		answer = answer.slice(0, maxNo);
 
 		searchTitle = "Top ";
 		noResult = maxNo;
 
-
 	} 
 
-	answer = answer.sort(function (x, y) {
-	    return y.subsidy - x.subsidy;
-	});
+	totalSubsidy = answer.reduce(function(sumAll,datum){ return sumAll + parseInt(datum.subsidy); }, 0);
+	totalStaff = answer.reduce(function(sumAll,datum){ return sumAll + parseInt(datum.employee); }, 0);
 
-	answer = answer.map(function(record){
-		record.subsidy = Intl.NumberFormat().format(record.subsidy);
-		return record;
-	});
-
-	// console.log(searchTitle);
-
-	res.render('list', {companyRecords: answer, 
-						searchTitle: searchTitle,
-						noResult: noResult
-					});
+	res.redirect("/");
 
 });
-
 
 app.listen(3000, function(){
 	console.log("server started on port 3000");
 });
+
+function sortAnswer(data, column, asc=false){
+	if (!asc) {
+		data = data.sort(function (x, y) {
+		    return y[column] - x[column];
+		});
+	} else {
+		data = data.sort(function (x, y) {
+		    return x[column] - y[column];
+		});
+	}
+	return data;
+}
